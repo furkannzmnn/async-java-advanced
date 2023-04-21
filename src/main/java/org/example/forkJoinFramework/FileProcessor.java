ba28c4bc9fafe5ade2c77b6bd3da398bb7f0f629
furkanozmen <ozmenf97@gmail.com>
2023-04-21 09:57:08 +0300

    init

--- src/main/java/org/example/forkJoinFramework/FileProcessor.java
+++ src/main/java/org/example/forkJoinFramework/FileProcessor.java
@@ -3,10 +3,12 @@ package org.example.forkJoinFramework;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 public class FileProcessor extends RecursiveTask<Integer> {
+    private final ExecutorService executorService = ForkJoinPool.commonPool();
 
     private static final long serialVersionUID = 1L;
     private static final int THRESHOLD = 1000;
@@ -34,5 +36,36 @@ public class FileProcessor extends RecursiveTask<Integer> {
         fileProcessor.compute();
         long endTime = System.currentTimeMillis();
         System.out.println("Time taken one method: " + (endTime - startTime));
+    }
+}
+
+class FileProcessorStreamParalelizm {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/lorem"))) {
+            long count = br.lines().parallel().map(line -> line.split(" ").length).reduce(0, Integer::sum);
+            System.out.println(count);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken stream method: " + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+        int count = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/lorem"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split(" ");
+                count += words.length;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Time taken concurrent method: " + (endTime - startTime));
+
+
     }
 }
